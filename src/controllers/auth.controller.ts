f1a7ba92898e7cf53220bfa514ae056550f2e0f6
Le Hoang Trong <langtupro0456@gmail.com>
2024-06-12 13:55:16 +0700

    hot fix exp refreshToken

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,9 @@
-import envConfig from '@/config'
 import prisma from '@/database'
 import { LoginBodyType } from '@/schemaValidations/auth.schema'
 import { RoleType, TokenPayload } from '@/types/jwt.types'
 import { comparePassword } from '@/utils/crypto'
 import { AuthError, EntityError } from '@/utils/errors'
 import { signAccessToken, signRefreshToken, verifyRefreshToken } from '@/utils/jwt'
-import { addMilliseconds } from 'date-fns'
 import ms from 'ms'
 
 export const logoutController = async (refreshToken: string) => {
@@ -38,7 +36,9 @@ export const loginController = async (body: LoginBodyType) => {
     userId: account.id,
     role: account.role as RoleType
   })
-  const refreshTokenExpiresAt = addMilliseconds(new Date(), ms(envConfig.REFRESH_TOKEN_EXPIRES_IN))
+
+  const decodedRefreshToken = verifyRefreshToken(refreshToken)
+  const refreshTokenExpiresAt = new Date(decodedRefreshToken.exp * 1000)
 
   await prisma.refreshToken.create({
     data: {
@@ -74,15 +74,11 @@ export const refreshTokenController = async (refreshToken: string) => {
     userId: account.id,
     role: account.role as RoleType
   })
-  const newRefreshToken = signRefreshToken(
-    {
-      userId: account.id,
-      role: account.role as RoleType
-    },
-    {
-      expiresIn: decodedRefreshToken.exp
-    }
-  )
+  const newRefreshToken = signRefreshToken({
+    userId: account.id,
+    role: account.role as RoleType,
+    exp: decodedRefreshToken.exp
+  })
   // Xóa refreshToken đi trước
   await prisma.refreshToken.delete({
     where: {
